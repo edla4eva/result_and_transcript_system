795fffa92f82d566b23d4114cb9483dc5713681f
User <edla4eva@yahoo.com>
2021-07-19 17:34:23 -0800

    Desktop App Source - Early beginings

---
+++ desktop_src/result_and_transcript_system/result_and_transcript_system/FormGenerateBroadsheet.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormGenerateBroadsheet
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxDepartment = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBoxLevel = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ButtonBackupBroadsheet = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBoxSession = New System.Windows.Forms.TextBox()
+        Me.ComboBoxDepartments = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ButtonProcessBroadsheet = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(29, 158)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(541, 187)
+        Me.DataGridView1.TabIndex = 9
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(29, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Department"
+        '
+        'TextBoxDepartment
+        '
+        Me.TextBoxDepartment.Location = New System.Drawing.Point(29, 31)
+        Me.TextBoxDepartment.Name = "TextBoxDepartment"
+        Me.TextBoxDepartment.Size = New System.Drawing.Size(123, 20)
+        Me.TextBoxDepartment.TabIndex = 12
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(41, 358)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(557, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "This broadsheet can be used to make final adjustments to scores as approved by de" &
+    "partmental board or facuty board"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(29, 54)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(33, 13)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Level"
+        '
+        'TextBoxLevel
+        '
+        Me.TextBoxLevel.Location = New System.Drawing.Point(29, 73)
+        Me.TextBoxLevel.Name = "TextBoxLevel"
+        Me.TextBoxLevel.Size = New System.Drawing.Size(123, 20)
+        Me.TextBoxLevel.TabIndex = 15
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 390)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 13)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "Filename (Excel file .xlsx)"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(32, 409)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(362, 20)
+        Me.TextBox1.TabIndex = 18
+        '
+        'ButtonBackupBroadsheet
+        '
+        Me.ButtonBackupBroadsheet.Location = New System.Drawing.Point(429, 389)
+        Me.ButtonBackupBroadsheet.Name = "ButtonBackupBroadsheet"
+        Me.ButtonBackupBroadsheet.Size = New System.Drawing.Size(141, 40)
+        Me.ButtonBackupBroadsheet.TabIndex = 17
+        Me.ButtonBackupBroadsheet.Text = "Backup Broadsheet"
+        Me.ButtonBackupBroadsheet.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(265, 465)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(141, 40)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(429, 465)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(141, 40)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Apply"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(29, 102)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(44, 13)
+        Me.Label5.TabIndex = 23
+        Me.Label5.Text = "Session"
+        '
+        'TextBoxSession
+        '
+        Me.TextBoxSession.Location = New System.Drawing.Point(29, 121)
+        Me.TextBoxSession.Name = "TextBoxSession"
+        Me.TextBoxSession.Size = New System.Drawing.Size(123, 20)
+        Me.TextBoxSession.TabIndex = 22
+        '
+        'ComboBoxDepartments
+        '
+        Me.ComboBoxDepartments.FormattingEnabled = True
+        Me.ComboBoxDepartments.Items.AddRange(New Object() {"Computer Engineering", "Production Engineering"})
+        Me.ComboBoxDepartments.Location = New System.Drawing.Point(159, 29)
+        Me.ComboBoxDepartments.Name = "ComboBoxDepartments"
+        Me.ComboBoxDepartments.Size = New System.Drawing.Size(161, 21)
+        Me.ComboBoxDepartments.TabIndex = 24
+        Me.ComboBoxDepartments.Text = "Computer Engineering"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"100", "200", "300", "400", "500", "600", "700", "800", "900"})
+        Me.ComboBox1.Location = New System.Drawing.Point(159, 73)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(161, 21)
+        Me.ComboBox1.TabIndex = 25
+        Me.ComboBox1.Text = "100"
+        '
+        'ButtonProcessBroadsheet
+        '
+        Me.ButtonProcessBroadsheet.Location = New System.Drawing.Point(402, 31)
+        Me.ButtonProcessBroadsheet.Name = "ButtonProcessBroadsheet"
+        Me.ButtonProcessBroadsheet.Size = New System.Drawing.Size(141, 110)
+        Me.ButtonProcessBroadsheet.TabIndex = 26
+        Me.ButtonProcessBroadsheet.Text = "Process Broadsheet"
+        Me.ButtonProcessBroadsheet.UseVisualStyleBackColor = True
+        '
+        'FormGenerateBroadsheet
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 517)
+        Me.Controls.Add(Me.ButtonProcessBroadsheet)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ComboBoxDepartments)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBoxSession)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ButtonBackupBroadsheet)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBoxLevel)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxDepartment)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "FormGenerateBroadsheet"
+        Me.Text = "Form Generate Broadsheet"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxDepartment As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBoxLevel As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ButtonBackupBroadsheet As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBoxSession As TextBox
+    Friend WithEvents ComboBoxDepartments As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ButtonProcessBroadsheet As Button
+End Class
---
+++ desktop_src/result_and_transcript_system/result_and_transcript_system/FormUploadResult.vb
@@ -0,0 +1,35 @@
+﻿Public Class FormUploadResult
+    Private Sub FormUploadResult_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.MdiParent = MDIParent1Main
+    End Sub
+
+    Private Sub ButtonBrowse_Click(sender As Object, e As EventArgs) Handles ButtonBrowse.Click
+        Dim FileOpenDialogBroadsheet As New OpenFileDialog()
+        Dim resultFileName As String = ""
+
+        If Not FileOpenDialogBroadsheet.ShowDialog = DialogResult.Cancel Then
+            resultFileName = FileOpenDialogBroadsheet.FileName()
+        End If
+        TextBoxExcelFilename.Text = resultFileName
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBoxExcelFilename.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+End Class
